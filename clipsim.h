3b26b685a8fdbf51d152ef6e3500378d6b60e61f
lucas-mior <lucas.mior@tutamail.com>
2023-04-21 21:43:50 -0300

    complete typedefs

--- clipsim.h
+++ clipsim.h
@@ -43,16 +43,18 @@ typedef enum Command {
     SAVE = 's',
 } Command;
 
-typedef unsigned long ulong;
-typedef unsigned int uint;
 typedef unsigned char uchar;
+typedef unsigned int uint;
+typedef unsigned long ulong;
 
-typedef int64_t int64;
-typedef int32_t int32;
+typedef int8_t int8;
 typedef int16_t int16;
-typedef uint64_t uint64;
-typedef uint32_t uint32;
+typedef int32_t int32;
+typedef int64_t int64;
+typedef uint8_t uint8;
 typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
 
 /* Maximum size for a single clipboard Entry, in bytes.
  * In practice the maximum size will be smaller due to
