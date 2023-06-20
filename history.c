4bab7a733566716580cd0a21b0b1ed5ba8c84862
lucas-mior <lucas.mior@tutamail.com>
2023-06-20 01:51:07 -0300

    clean up

--- history.c
+++ history.c
@@ -49,23 +49,19 @@ void history_save_entry(Entry *e) {
         }
         if (write(history.fd, image_save, (size_t) length) < 0) {
             printf("Error writing %s: %s\n", image_save, strerror(errno));
-            system("dunstify \"clipsim\" \"Error writing image_save!\"");
             exit(EXIT_FAILURE);
         }
         if (write(history.fd, &IMAGE_END, 1) < 0) {
             printf("Error writing IMAGE_END: %s\n", strerror(errno));
-            system("dunstify \"clipsim\" \"Error writing IMAGE_END!\"");
             exit(EXIT_FAILURE);
         }
     } else {
         if (write(history.fd, e->content, e->content_length) < 0) {
             printf("Error writing %s: %s\n", e->content, strerror(errno));
-            system("dunstify \"clipsim\" \"Error writing e->content!\"");
             exit(EXIT_FAILURE);
         }
         if (write(history.fd, &TEXT_END, 1) < 0) {
             printf("Error writing TEXT_END: %s\n", strerror(errno));
-            system("dunstify \"clipsim\" \"Error writing TEXT_END!\"");
             exit(EXIT_FAILURE);
         }
     }
