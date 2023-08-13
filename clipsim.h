5ab0cb14c8f23daed933e5617c9f566a3c28f409
lucas-mior <lucas.mior@tutamail.com>
2023-08-13 14:35:53 -0300

    organize header file

--- clipsim.h
+++ clipsim.h
@@ -95,9 +95,6 @@ enum {
     COMMAND_HELP,
 };
 
-static const char TEXT_END = (char) 0x01;
-static const char IMAGE_END = (char) 0x02;
-
 typedef struct Command {
     const char *shortname;
     const char *longname;
@@ -116,6 +113,8 @@ static const Command commands[] = {
 
 extern Entry entries[HISTORY_BUFFER_SIZE];
 extern mtx_t lock;
+static const char TEXT_END = (char) 0x01;
+static const char IMAGE_END = (char) 0x02;
 
 void content_remove_newline(char *, size_t *);
 void content_trim_spaces(char **, size_t *, char *, size_t);
