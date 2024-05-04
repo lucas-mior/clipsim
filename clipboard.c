f43f80816f95a7caa970812d41abd5e91a3c3641
lucas-mior <lucas.mior@tutamail.com>
2024-05-04 12:24:20 -0300

    update copyright

--- clipboard.c
+++ clipboard.c
@@ -1,5 +1,5 @@
 /* This file is part of clipsim.
- * Copyright (C) 2023 Lucas Mior
+ * Copyright (C) 2024 Lucas Mior
 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
