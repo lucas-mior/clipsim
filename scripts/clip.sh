d6c56b206d31b6efebe4f83398574c0afad4fb1a
lucas-mior <lucas.mior@tutamail.com>
2024-06-11 12:01:02 -0300

    use --no-multi-line fzf option on script

--- scripts/clip.sh
+++ scripts/clip.sh
@@ -4,6 +4,7 @@
 # usage: $0 [--copy|--delete]
 
 id="$(clipsim --print 2> /dev/null \
-    | fzf --prompt="clipsim $1 " --reverse --read0 --preview='clipinfo.sh {}' \
+    | fzf --prompt="clipsim $1 " \
+          --reverse --no-multi-line --read0 --preview='clipinfo.sh {}' \
     | awk 'NR==1{print $1; exit}')"
 [ -n "$id" ] && clipsim "$1" "$id"
