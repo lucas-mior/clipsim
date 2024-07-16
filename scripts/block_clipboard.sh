6be2455dd991f2eec6b5ea916c85e79596079ed1
lucas-mior <lucas.mior@tutamail.com>
2024-07-16 16:35:17 -0300

    update block_clipboard.sh

--- scripts/block_clipboard.sh
+++ scripts/block_clipboard.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# clipboard for dwmblocks
+# clipboard for dwmblocks2
 
 display () {
     printf "📋 "
@@ -8,21 +8,22 @@ display () {
     result="$(timeout 0.5s xclip -selection clipboard -o -t TARGETS)"
 
     if echo "$result" | grep -q "image/png"; then
-        image=true
         printf "PNG\n"
     elif [ -n "$result" ]; then
-        content="$(timeout 1s dash -c "xclip -selection clipboard -o | head -n 100")"
-        /usr/bin/echo "$content" \
-            | sed -zE 's/^\s+//g;
-                       s/([^^])\s+/\1 /g;
-                       s/\s+$//g;
-                       s/(.{14}\S).{4,}(\S.{5})/\1 ... \2/g;'
+        content="$(timeout 1s dash -c \
+                   "xclip -selection clipboard -o | head -n 100")"
+        echo "$content" \
+            | tr '\n\0' ' ' \
+            | sed -E 's/^\s+//g;
+                      s/([^^])\s+/\1 /g;
+                      s/\s+$//g;
+                      s/(.{14}\S).{4,}(\S.{5})/\1 ... \2/g;'
     fi
 }
 
-case "$1" in
-    1) pgrep clipsim || setsid -f clipsim -d ;;
-    2) clipsim --save  && dunstify "📋 clipsim" "History saved" ;;
-    6) setsid -f "$TERMINAL" -e "$EDITOR" "$0" ;;
+case $1 in
+    1) pgrep clipsim  > /dev/null || setsid -f clipsim -d                  ; display ;;
+    2) clipsim --save > /dev/null && dunstify "📋 clipsim" "History saved" ; display ;;
+    6) setsid -f "$TERMINAL" -e "$EDITOR" "$0"                                 ; display ;;
     *) display ;;
 esac
