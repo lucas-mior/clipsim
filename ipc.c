4aa0dd6d7336c40f6f1cd752233d4731d7c43023
lucas-mior <lucas.mior@tutamail.com>
2025-03-05 10:17:34 -0300

    better empty history handling

--- ipc.c
+++ ipc.c
@@ -202,8 +202,6 @@ ipc_daemon_pipe_entries(void) {
 
     if (history_length <= 0) {
         error("Clipboard history empty. Start copying text.\n");
-        fprintf(content_fifo.file,
-                "000 Clipboard history empty. Start copying text.\n");
         goto close;
     }
 
