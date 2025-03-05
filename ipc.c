1b8a529c8b48e08c7ac95baaf47e70cf873cc84e
lucas-mior <lucas.mior@tutamail.com>
2025-03-04 21:40:05 -0300

    style

--- ipc.c
+++ ipc.c
@@ -210,6 +210,7 @@ ipc_daemon_pipe_entries(void) {
     for (int32 i = history_length - 1; i >= 0; i -= 1) {
         Entry *e = &entries[i];
         usize size = (usize) e->trimmed_length + 1;
+
         fprintf(content_fifo.file, "%.*d ", PRINT_DIGITS, i);
         if (fwrite(&e->content[e->trimmed], 1, size, content_fifo.file) < size) {
             error("Error writing to client fifo.\n");
