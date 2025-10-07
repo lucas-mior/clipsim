91a1fde7d2db2dd2251eda647eba62c822617f03
lucas-mior <lucas.mior@tutamail.com>
2025-10-07 12:33:25 -0300

    dont use libev for xmousepasteblock.c

--- xmousepasteblock.c
+++ xmousepasteblock.c
@@ -21,143 +21,95 @@
  */
 
 #include <stdlib.h>
-#include <ev.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/select.h>
 
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
 #include <X11/extensions/XInput2.h>
 
-#include "util.c"
+#define error(...) fprintf(stderr, __VA_ARGS__)
 
 static Display *display;
 static int xi_opcode = -1;
 
-void stub_cb(EV_P_ ev_io *w, int revents) {
-    (void) w;
-    (void) revents;
-    return;
-}
-
-void check_cb(EV_P_ ev_check *w, int revents) {
+static void handle_xinput_events(void) {
     XEvent xevent;
-    (void) w;
-    (void) revents;
-
     while (XPending(display) > 0) {
         XNextEvent(display, &xevent);
         XGenericEventCookie *cookie = &xevent.xcookie;
-        if (cookie->type != GenericEvent
-            || cookie->extension !=  xi_opcode
-            || !XGetEventData(display, cookie)) {
-#ifdef DEBUG
-            error("DEBUG: Dropping event of type %i", cookie->type);
-#endif
+
+        if (cookie->type != GenericEvent ||
+            cookie->extension != xi_opcode ||
+            !XGetEventData(display, cookie))
             continue;
-        }
 
         const XIRawEvent *data = (const XIRawEvent *) cookie->data;
-#ifdef DEBUG
-        error("DEBUG: Registered button press %i on device %i (source device %i)\n", data->detail, data->deviceid, data->sourceid);
-#endif
-        if (data->detail == 2) {
-            /* Clear primary selection */
-            XSetSelectionOwner(display, XA_PRIMARY, None, CurrentTime);
 
-            /* Also clear deprecated cut buffer */
+        if (data->detail == 2) {  // middle mouse button
+            XSetSelectionOwner(display, XA_PRIMARY, None, CurrentTime);
             XStoreBytes(display, None, 0);
             XSetSelectionOwner(display, XA_STRING, None, CurrentTime);
-
             XSync(display, False);
-#ifdef DEBUG
-            error("DEBUG: Primary selection and cut buffer cleared\n");
-#endif
-            return;
+            error("Cleared primary selection and cut buffer\n");
         }
 
         XFreeEventData(display, cookie);
     }
-    return;
 }
 
 int main(int argc, const char* argv[]) {
-    struct ev_loop *ev_loop = EV_DEFAULT;
     int watch_slave_devices = 0;
-    int event;
-    int error_num;
-    char *XMPB_WATCH_SLAVE_DEVICES;
-
     (void) argc;
     (void) argv;
+    char *env = getenv("XMPB_WATCH_SLAVE_DEVICES");
+    if (env && (strcmp(env, "1") == 0 || strcasecmp(env, "true") == 0))
+        watch_slave_devices = 1;
 
-    if ((display = XOpenDisplay(NULL)) == NULL) {
-        error("Error: Failed to connect to the X server\n");
-        exit(EXIT_FAILURE);
+    display = XOpenDisplay(NULL);
+    if (!display) {
+        error("Failed to connect to X server\n");
+        return 1;
     }
 
-    if ((XMPB_WATCH_SLAVE_DEVICES = getenv("XMPB_WATCH_SLAVE_DEVICES"))) {
-        for (char *c = XMPB_WATCH_SLAVE_DEVICES; *c; c += 1) {
-           *c = *c > 0x40 && *c < 0x5b ? *c | 0x60 : *c;
-        }
-        if (strcmp(XMPB_WATCH_SLAVE_DEVICES, "1") == 0
-            || strcmp(XMPB_WATCH_SLAVE_DEVICES, "true") == 0) {
-            watch_slave_devices = 1;
-        }
+    int event, error_num;
+    if (!XQueryExtension(display, "XInputExtension", &xi_opcode, &event, &error_num)) {
+        error("XInput extension not available\n");
+        return 1;
     }
 
-    if (!XQueryExtension(display,
-                         "XInputExtension", &xi_opcode, &event, &error_num)) {
-        error("Error: XInput extension not available\n");
-        exit(EXIT_FAILURE);
+    int major = 2, minor = 2;
+    if (XIQueryVersion(display, &major, &minor) != Success) {
+        error("XI2 >= 2.2 required\n");
+        return 1;
     }
 
-    {
-        int major_op = 2;
-        int minor_op = 2;
-        int result;
-        if ((result = XIQueryVersion(display, &major_op, &minor_op)) == BadRequest) {
-            error("Error: XI2 >=%d.%d required).\n", major_op, minor_op);
-            exit(EXIT_FAILURE);
-        } else if (result != Success) {
-            error("Error: Failed to query XI2\n");
-            exit(EXIT_FAILURE);
-        }
-    }
-    XIEventMask masks[1];
+    XIEventMask mask;
+    unsigned char mask_bits[(XI_LASTEVENT + 7)/8];
+    memset(mask_bits, 0, sizeof(mask_bits));
 
-    {
-        unsigned char mask_master[(XI_LASTEVENT + 7)/8];
-        memset(mask_master, 0, sizeof(mask_master));
-        masks[0].mask_len = sizeof(mask_master);
-        masks[0].mask = mask_master;
-        if (watch_slave_devices) {
-            masks[0].deviceid = XIAllDevices;
-            XISetMask(mask_master, XI_ButtonPress);
-        } else {
-            masks[0].deviceid = XIAllMasterDevices;
-            XISetMask(mask_master, XI_RawButtonPress);
-        }
-    }
+    mask.deviceid = watch_slave_devices ? XIAllDevices : XIAllMasterDevices;
+    mask.mask_len = sizeof(mask_bits);
+    mask.mask = mask_bits;
+    XISetMask(mask_bits, watch_slave_devices ? XI_ButtonPress : XI_RawButtonPress);
 
-    XISelectEvents(display, DefaultRootWindow(display), masks, 1);
+    XISelectEvents(display, DefaultRootWindow(display), &mask, 1);
     XFlush(display);
-
-    {
-        struct ev_io *x_watcher;
-        struct ev_check *x_check;
-
-        x_watcher = util_calloc(1, sizeof(*x_watcher));
-        ev_io_init(x_watcher, stub_cb, XConnectionNumber(display), EV_READ);
-        ev_io_start(ev_loop, x_watcher);
-    
-        x_check = util_calloc(1, sizeof(*x_check));
-        ev_check_init(x_check, check_cb);
-        ev_check_start(ev_loop, x_check);
-
-    }
 
     error("Blocking new mouse paste actions from all %s devices\n",
           watch_slave_devices ? "slave" : "master");
-    ev_run(ev_loop, 0);
 
-    exit(EXIT_SUCCESS);
+    int xfd = XConnectionNumber(display);
+    while (1) {
+        fd_set fds;
+        FD_ZERO(&fds);
+        FD_SET(xfd, &fds);
+
+        if (select(xfd + 1, &fds, NULL, NULL, NULL) > 0)
+            handle_xinput_events();
+    }
+
+    return 0;
 }
