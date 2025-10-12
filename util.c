6aece5f47354269d366ced3faac14c33b369e6f4
lucas-mior <lucas.mior@tutamail.com>
2025-10-11 21:17:06 -0300

    put send_signal in util.c

--- util.c
+++ util.c
@@ -103,6 +103,7 @@ static int util_command(const int, char **);
 static uint32 util_nthreads(void);
 static void util_die_notify(const char *, ...) __attribute__((noreturn));
 static void util_segv_handler(int32) __attribute__((noreturn));
+static void send_signal(const char *, const int);
 
 static size_t util_page_size = 0;
 
@@ -534,6 +535,73 @@ util_copy_file(const char *destination, const char *source) {
     close(destination_fd);
     return 0;
 }
+
+#ifdef __linux__
+#include <dirent.h>
+void
+send_signal(const char *executable, const int32 signal_number) {
+    DIR *processes;
+    struct dirent *process;
+
+    if ((processes = opendir("/proc")) == NULL) {
+        error("Error opening /proc: %s\n", strerror(errno));
+        return;
+    }
+
+    while ((process = readdir(processes))) {
+        static char buffer[256];
+        static char command[256];
+        int32 pid;
+        int32 cmdline;
+
+        if (process->d_type != DT_DIR)
+            continue;
+        if ((pid = atoi(process->d_name)) <= 0)
+            continue;
+
+        SNPRINTF(buffer, "/proc/%s/cmdline", process->d_name);
+
+        if ((cmdline = open(buffer, O_RDONLY)) < 0)
+            continue;
+
+        if (read(cmdline, command, sizeof(command)) <= 0) {
+            close(cmdline);
+            continue;
+        }
+        if (!strcmp(command, executable)) {
+            if (kill(pid, signal_number) < 0) {
+                error("Error sending signal %d to program %s (pid %d): %s.\n",
+                      signal_number, executable, pid, strerror(errno));
+            }
+        }
+
+        close(cmdline);
+    }
+
+    closedir(processes);
+    return;
+}
+#else
+void
+send_signal(const char *executable, const int32 signal_number) {
+    char signal_string[14];
+    int32 n;
+    SNPRINTF(signal_string, "%d", signal_number);
+
+    switch (fork()) {
+        case -1:
+            error("Error forking: %s\n", strerror(errno));
+            return;
+        case 0:
+            execlp("pkill", "pkill", signal_string, executable, NULL);
+            error("Error executing pkill: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        default:
+            wait(NULL);
+    }
+    return;
+}
+#endif
 
 #ifdef TESTING_util
 #include <assert.h>
