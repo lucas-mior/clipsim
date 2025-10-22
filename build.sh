d6d830a9262bfd89030bbdb6ce123ec62dede973
lucas-mior <lucas.mior@tutamail.com>
2025-10-22 09:35:34 -0300

    style

--- build.sh
+++ build.sh
@@ -42,36 +42,36 @@ case "$target" in
 esac
 
 case "$target" in
-    "uninstall")
-        set -x
-        rm -f ${DESTDIR}${PREFIX}/bin/${program}
-        rm -f ${DESTDIR}${PREFIX}/share/man/man1/${program}.1
-        rm -f ${DESTDIR}${PREFIX}/share/fish/vendor_completions.d/${program}.fish
-        rm -f ${DESTDIR}${PREFIX}/share/bash-completion/completions/${program}
-        rm -f ${DESTDIR}${PREFIX}/share/zsh/site-functions/_${program}
-        rm -f ${DESTDIR}${PREFIX}/share/licenses/${program}/LICENSE
-        ;;
-    "install")
-        [ ! -f "$program" ] && "$0" build
-        set -x
-        install -Dm755 ${program}                  ${DESTDIR}${PREFIX}/bin/${program}
-        install -Dm644 ${program}.1                ${DESTDIR}${PREFIX}/man/man1/${program}.1
-        install -Dm644 completions/${program}.fish ${DESTDIR}${PREFIX}/share/fish/vendor_completions.d/${program}.fish
-        install -Dm644 completions/${program}.bash ${DESTDIR}${PREFIX}/share/bash-completion/completions/${program}
-        install -Dm644 completions/${program}.zsh  ${DESTDIR}${PREFIX}/share/zsh/site-functions/_${program}
-        install -Dm644 LICENSE                     ${DESTDIR}${PREFIX}/share/licenses/${program}/LICENSE
-        ;;
-    "clean")
-        set -x
-        rm -f *.o *~ ${program}
-        ;;
-    "build"|"debug"|"release")
-        ctags --kinds-C=+l ./*.h ./*.c 2>/dev/null || true
-        vtags.sed tags > .tags.vim 2>/dev/null || true
-        set -x
-        $CC $CFLAGS $LDFLAGS -o ${program} ${src} ${LDLIBS}
-        ;;
-    *)
-        echo "usage: $0 [ build | debug | release | clang | install | uninstall | clean ]"
-        ;;
+"uninstall")
+    set -x
+    rm -f ${DESTDIR}${PREFIX}/bin/${program}
+    rm -f ${DESTDIR}${PREFIX}/share/man/man1/${program}.1
+    rm -f ${DESTDIR}${PREFIX}/share/fish/vendor_completions.d/${program}.fish
+    rm -f ${DESTDIR}${PREFIX}/share/bash-completion/completions/${program}
+    rm -f ${DESTDIR}${PREFIX}/share/zsh/site-functions/_${program}
+    rm -f ${DESTDIR}${PREFIX}/share/licenses/${program}/LICENSE
+    ;;
+"install")
+    [ ! -f "$program" ] && "$0" build
+    set -x
+    install -Dm755 ${program}                  ${DESTDIR}${PREFIX}/bin/${program}
+    install -Dm644 ${program}.1                ${DESTDIR}${PREFIX}/man/man1/${program}.1
+    install -Dm644 completions/${program}.fish ${DESTDIR}${PREFIX}/share/fish/vendor_completions.d/${program}.fish
+    install -Dm644 completions/${program}.bash ${DESTDIR}${PREFIX}/share/bash-completion/completions/${program}
+    install -Dm644 completions/${program}.zsh  ${DESTDIR}${PREFIX}/share/zsh/site-functions/_${program}
+    install -Dm644 LICENSE                     ${DESTDIR}${PREFIX}/share/licenses/${program}/LICENSE
+    ;;
+"clean")
+    set -x
+    rm -f *.o *~ ${program}
+    ;;
+"build"|"debug"|"release")
+    ctags --kinds-C=+l ./*.h ./*.c 2>/dev/null || true
+    vtags.sed tags > .tags.vim 2>/dev/null || true
+    set -x
+    $CC $CFLAGS $LDFLAGS -o ${program} ${src} ${LDLIBS}
+    ;;
+*)
+    echo "usage: $0 [ build | debug | release | clang | install | uninstall | clean ]"
+    ;;
 esac
