f110580b4078fbf88c8e10dd2852f8bbe4d7d090
lucas-mior <lucas.mior@tutamail.com>
2025-11-04 17:47:14 -0300

    debug

--- content.c
+++ content.c
@@ -27,7 +27,7 @@ static int32 content_check_content(uchar *, int);
 
 void
 content_remove_newline(char *text, int32 *length) {
-    DEBUG_PRINT("%s, %d", text, *length)
+    /* DEBUG_PRINT("%s, %d", text, *length) */
     text[*length] = '\0';
     while (text[*length - 1] == '\n') {
         text[*length - 1] = '\0';
@@ -39,8 +39,8 @@ content_remove_newline(char *text, int32 *length) {
 void
 content_trim_spaces(int16 *trimmed, int16 *trimmed_length, char *content,
                     const int32 length) {
-    DEBUG_PRINT("%p, %p, %s, %d", (void *)trimmed, (void *)trimmed_length,
-                content, length)
+    /* DEBUG_PRINT("%p, %p, %s, %d", (void *)trimmed, (void *)trimmed_length, */
+    /*             content, length) */
     char *out;
     char temp = '\0';
     char *in = content;
