efa380991d5b8e6e24755d68cfd2a43e5bb2c561
lucas-mior <lucas.mior@tutamail.com>
2025-11-05 07:15:48 -0300

    use more compreensive build.sh from brn2

--- build.sh
+++ build.sh
@@ -1,77 +1,197 @@
-#!/bin/sh
+#!/bin/sh -e
 
 # shellcheck disable=SC2086
 
+alias trace_on='set -x'
+alias trace_off='{ set +x; } 2>/dev/null'
+
+. ./targets
+
+dir="$(realpath "$(dirname "$0")")"
+
 target="${1:-build}"
+
+if ! grep -q "$target" ./targets; then
+    echo "usage: $(basename "$0") <targets>"
+    cat targets
+    exit 1
+fi
+
+cross="$2"
+
+printf "\n${0} ${RED}${1} ${2}$RES\n"
 PREFIX="${PREFIX:-/usr/local}"
 DESTDIR="${DESTDIR:-/}"
 
-src="main.c"
+main="main.c"
 program="clipsim"
+exe="bin/$program"
+mkdir -p "$(dirname "$exe")"
+
+CFLAGS="$CFLAGS -std=c99"
+CFLAGS="$CFLAGS -Wextra -Wall"
+CFLAGS="$CFLAGS -Wno-unknown-warning-option"
+CFLAGS="$CFLAGS -Wno-unused-macros -Wno-unused-function"
+CFLAGS="$CFLAGS -Wno-constant-logical-operand"
+CFLAGS="$CFLAGS -Wno-unknown-pragmas"
+CFLAGS="$CFLAGS -Wfatal-errors"
+CPPFLAGS="$CPPFLAGS -D_DEFAULT_SOURCE -D_XOPEN_SOURCE=600"
+
+LDFLAGS="$LDFLAGS $(pkg-config x11 --libs)"
+LDFLAGS="$LDFLAGS $(pkg-config xfixes --libs)"
+LDFLAGS="$LDFLAGS $(pkg-config xi --libs)"
+LDFLAGS="$LDFLAGS $(pkg-config libmagic --libs)"
+
+OS=$(uname -a)
 
 CC=${CC:-cc}
 
-LDLIBS="$LDLIBS $(pkg-config x11 --libs)"
-LDLIBS="$LDLIBS $(pkg-config xfixes --libs)"
-LDLIBS="$LDLIBS $(pkg-config xi --libs)"
-LDLIBS="$LDLIBS $(pkg-config libmagic --libs)"
-LDLIBS="$LDLIBS -lpthread"
+case "$target" in
+"debug")
+    CFLAGS="$CFLAGS -g -fsanitize=undefined"
+    CPPFLAGS="$CPPFLAGS $GNUSOURCE -DDEBUGGING=1"
+    exe="bin/${program}_debug"
+    ;;
+"valgrind") 
+    CFLAGS="$CFLAGS -g -O0 -ftree-vectorize"
+    CPPFLAGS="$CPPFLAGS $GNUSOURCE -DDEBUGGING=1"
+    ;;
+"check") 
+    CC=gcc
+    CFLAGS="$CFLAGS $GNUSOURCE -fanalyzer"
+    ;;
+"build") 
+    CFLAGS="$CFLAGS $GNUSOURCE -O2 -flto -march=native -ftree-vectorize"
+    ;;
+*)
+    CFLAGS="$CFLAGS -O2"
+    ;;
+esac
 
-CFLAGS="$CFLAGS -std=c99 -D_DEFAULT_SOURCE -D_XOPEN_SOURCE=600"
-CFLAGS="$CFLAGS -Wall -Wextra"
-CFLAGS="$CFLAGS -Wno-unused-function -Wno-constant-logical-operand"
+if [ "$target" = "cross" ]; then
+    CC="zig cc"
+    CFLAGS="$CFLAGS -target $cross"
 
-if [ "$CC" = "clang" ]; then
+    case $cross in
+    "x86_64-macos"|"aarch64-macos")
+        CFLAGS="$CFLAGS -fno-lto"
+        LDFLAGS="$LDFLAGS -lpthread"
+        ;;
+    *windows*)
+        exe="bin/$program.exe"
+        ;;
+    *)
+        LDFLAGS="$LDFLAGS -lpthread"
+        ;;
+    esac
+else
+    LDFLAGS="$LDFLAGS -lpthread"
+fi
+
+if [ "$target" != "test" ] && [ "$CC" = "clang" ]; then
     CFLAGS="$CFLAGS -Weverything"
     CFLAGS="$CFLAGS -Wno-unsafe-buffer-usage"
     CFLAGS="$CFLAGS -Wno-format-nonliteral"
-    CFLAGS="$CFLAGS -Wno-format-zero-length"
+    CFLAGS="$CFLAGS -Wno-disabled-macro-expansion"
+    CFLAGS="$CFLAGS -Wno-c++-keyword"
     CFLAGS="$CFLAGS -Wno-implicit-void-ptr-cast"
 fi
 
-echo "target=$target"
-
-case "$target" in
-    "debug")
-        CFLAGS="$CFLAGS -g -fsanitize=undefined -DDEBUGGING=1"
-        CFLAGS="$CFLAGS -Wno-format-zero-length"
-        ;;
-    "release"|"build")
-        CFLAGS="$CFLAGS -O2 -flto"
-        ;;
-esac
-
 case "$target" in
 "uninstall")
-    set -x
+    trace_on
     rm -f ${DESTDIR}${PREFIX}/bin/${program}
-    rm -f ${DESTDIR}${PREFIX}/share/man/man1/${program}.1
+    rm -f ${DESTDIR}${PREFIX}/man/man1/${program}.1
     rm -f ${DESTDIR}${PREFIX}/share/fish/vendor_completions.d/${program}.fish
     rm -f ${DESTDIR}${PREFIX}/share/bash-completion/completions/${program}
     rm -f ${DESTDIR}${PREFIX}/share/zsh/site-functions/_${program}
     rm -f ${DESTDIR}${PREFIX}/share/licenses/${program}/LICENSE
+    exit
     ;;
 "install")
-    [ ! -f "$program" ] && "$0" build
-    set -x
-    install -Dm755 ${program}                  ${DESTDIR}${PREFIX}/bin/${program}
+    [ ! -f $program ] && $0 build
+    trace_on
+    install -Dm755 bin/${program}              ${DESTDIR}${PREFIX}/bin/${program}
     install -Dm644 ${program}.1                ${DESTDIR}${PREFIX}/man/man1/${program}.1
     install -Dm644 completions/${program}.fish ${DESTDIR}${PREFIX}/share/fish/vendor_completions.d/${program}.fish
     install -Dm644 completions/${program}.bash ${DESTDIR}${PREFIX}/share/bash-completion/completions/${program}
     install -Dm644 completions/${program}.zsh  ${DESTDIR}${PREFIX}/share/zsh/site-functions/_${program}
     install -Dm644 LICENSE                     ${DESTDIR}${PREFIX}/share/licenses/${program}/LICENSE
+    exit
     ;;
-"clean")
-    set -x
-    rm -f *.o *~ ${program}
+"assembly")
+    trace_on
+    $CC $CPPFLAGS $CFLAGS -S -o ${program}_$CC.S "$main" $LDFLAGS
+    exit
     ;;
-"build"|"debug"|"release")
-    ctags --kinds-C=+l ./*.h ./*.c 2>/dev/null || true
-    vtags.sed tags > .tags.vim 2>/dev/null || true
-    set -x
-    $CC $CFLAGS $LDFLAGS -o ${program} ${src} ${LDLIBS}
+"test_all")
     ;;
 *)
-    echo "usage: $0 [ build | debug | release | clang | install | uninstall | clean ]"
+    trace_on
+    ctags --kinds-C=+l+d ./*.h ./*.c 2> /dev/null || true
+    vtags.sed tags > .tags.vim       2> /dev/null || true
+    $CC $CPPFLAGS $CFLAGS -o ${exe} "$main" $LDFLAGS
+    trace_off
     ;;
 esac
+
+create_temp_files() {
+    tmpdir="/tmp/clipsim"
+    rm -rf "$tmpdir"
+    mkdir -p "$tmpdir"
+    cd "$tmpdir" || exit
+
+    seq -w 100000 | sed 's/^/0011223344/g' | xargs -P"$(nproc)" touch
+}
+
+case "$target" in
+"benchmark") 
+    create_temp_files
+
+    # strace -f -c -o $dir/strace.txt $dir/clipsim -s -q -d . 2>&1
+    trace_on
+    $dir/$exe -s -q -d .
+    trace_off
+    rm $dir/$exe
+    exit
+    ;;
+"valgrind") 
+    vg_flags="--error-exitcode=1 --errors-for-leak-kinds=all"
+    vg_flags="$vg_flags --leak-check=full --show-leak-kinds=all"
+
+    trace_on
+    valgrind $vg_flags -s --tool=memcheck $dir/bin/clipsim -d
+    trace_off
+    exit
+    ;;
+"perf")
+    create_temp_files
+
+    cd /tmp/clipsim || exit
+    trace_on
+    perf record -b -o $dir/perf.data $dir/$exe -s -q -d .
+    cd "$dir"
+    perf annotate $dir/$exe
+    perf report -v perf.data
+    trace_off
+    exit
+    ;;
+"check")
+    scan-build --view -analyze-headers --status-bugs ./build.sh
+    exit
+    ;;
+esac
+
+trace_off
+if [ "$target" = "test_all" ]; then
+    printf '%s\n' "$targets" | while IFS= read -r target; do
+        echo "$target" | grep -Eq "^(# |$)" && continue
+        if echo "$target" | grep "cross"; then
+            $0 $target
+            continue
+        fi
+        for compiler in gcc tcc clang "zig cc" ; do
+            CC=$compiler $0 $target || exit
+        done
+    done
+fi
