3ee0a2b28c537eaff73f26bf544503281c6ad0ff
lucas-mior <lucas.mior@tutamail.com>
2025-11-21 18:59:55 -0300

    order of primitives

--- generic.c
+++ generic.c
@@ -97,9 +97,10 @@ _Generic((VARIABLE), \
 )
 
 // clang-format off
-static ldouble ldouble_from_ldouble(ldouble x) { return x;             }
-static ldouble ldouble_from_double(double x)   { return (ldouble)x;    }
-static ldouble ldouble_from_float(float x)     { return (ldouble)x;    }
+static ldouble ldouble_from_voidp(void *x)     { (void)x; return 0.0l; }
+static ldouble ldouble_from_charp(char *x)     { (void)x; return 0.0l; }
+static ldouble ldouble_from_bool(bool x)       { (void)x; return 0.0l; }
+static ldouble ldouble_from_char(char x)       { (void)x; return 0.0l; }
 static ldouble ldouble_from_schar(schar x)     { return (ldouble)x;    }
 static ldouble ldouble_from_short(short x)     { return (ldouble)x;    }
 static ldouble ldouble_from_int(int x)         { return (ldouble)x;    }
@@ -110,10 +111,9 @@ static ldouble ldouble_from_ushort(ushort x)   { return (ldouble)x;    }
 static ldouble ldouble_from_uint(uint x)       { return (ldouble)x;    }
 static ldouble ldouble_from_ulong(ulong x)     { return (ldouble)x;    }
 static ldouble ldouble_from_ullong(ullong x)   { return (ldouble)x;    }
-static ldouble ldouble_from_charp(char *x)     { (void)x; return 0.0l; }
-static ldouble ldouble_from_voidp(void *x)     { (void)x; return 0.0l; }
-static ldouble ldouble_from_bool(bool x)       { (void)x; return 0.0l; }
-static ldouble ldouble_from_char(char x)       { (void)x; return 0.0l; }
+static ldouble ldouble_from_float(float x)     { return (ldouble)x;    }
+static ldouble ldouble_from_double(double x)   { return (ldouble)x;    }
+static ldouble ldouble_from_ldouble(ldouble x) { return x;             }
 // clang-format on
 
 enum Type {
