a0c797214c77c6aa307285353495df75e466722e
lucas-mior <lucas.mior@tutamail.com>
2026-04-16 17:16:43 -0300

    put posix_fadvise with the open calls

--- cbase/util.c
+++ cbase/util.c
@@ -1050,7 +1050,7 @@ util_copy_file_async(char *destination, char *source, int *dest_fd) {
         return -1;
     }
 
-    if ((fadvise_err = posix_fadvise(pipes[nfds].fd,
+    if ((fadvise_err = posix_fadvise(source_fd,
                                      0, 0,
                                      POSIX_FADV_WILLNEED)) < 0) {
         error("Error in posix_fadvise(POSIX_FADV_WILLNEED): %s.\n",
