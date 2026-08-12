4a9a9e628fa2ca7ba001fbb95d67bee1920ae26c
lucas-mior <lucas.mior@tutamail.com>
2026-08-12 16:33:37 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -3,12 +3,11 @@
 # shellcheck disable=SC2086
 
 dir=$(dirname "$(readlink -f "$0")")
-# shellcheck source=/dev/null
-. "$dir/cbase/common.sh"
-
 cd "$dir" || exit
 
-cbase="cbase"
+# shellcheck source=./cbase/common.sh
+. "./cbase/common.sh"
+
 script=$(basename "$0")
 
 if [ -f ./targets ]; then
@@ -49,7 +48,7 @@ mkdir -p "$(dirname "$exe")"
 
 CC=$(common_get_compiler "$mode")
 
-CPPFLAGS="$CPPFLAGS -I$dir/$cbase"
+CPPFLAGS="$CPPFLAGS -Icbase"
 
 CFLAGS="$CFLAGS -std=c11"
 CFLAGS="$CFLAGS -Wfatal-errors"
