2dab182f747b202a3f42450376b545f82700c003
lucas-mior <lucas.mior@tutamail.com>
2026-08-13 16:33:54 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -42,7 +42,7 @@ CFLAGS="$CFLAGS -Wextra -Wall"
 CFLAGS="$CFLAGS -Werror=all -Werror=extra"
 CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 
-if [ "$CC" = "clang" ]; then
+if [ "$CC" = "clang" ] || [ "$CC" = "zig cc" ]; then
     CFLAGS="$CFLAGS -Weverything"
     CFLAGS="$CFLAGS -Wno-assign-enum"
     CFLAGS="$CFLAGS -Wno-c++-keyword"
