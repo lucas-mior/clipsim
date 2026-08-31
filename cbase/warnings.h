650e2dcd6eb479ed95c74efa75102de31fdab3b1
lucas-mior <lucas.mior@tutamail.com>
2026-08-30 21:03:06 -0300

    update cbase

--- cbase/warnings.h
+++ cbase/warnings.h
@@ -1,31 +1,38 @@
 #if !defined(WARNINGS_H)
 #define WARNINGS_H
 
+#include "platform_detection.h"
+
 #if CC_GCC || CC_CLANG
-#pragma GCC diagnostic warning "-Wall"
-#pragma GCC diagnostic warning "-Wextra"
+  #pragma GCC diagnostic warning "-Wall"
+  #pragma GCC diagnostic warning "-Wextra"
 #endif
 
 #if CC_CLANG
-#pragma clang diagnostic warning "-Weverything"
-#pragma clang diagnostic ignored "-Wconstant-logical-operand"
-#pragma clang diagnostic ignored "-Wassign-enum"
-#pragma clang diagnostic ignored "-Wc++-keyword"
-#pragma clang diagnostic ignored "-Wc++98-compat"
-#pragma clang diagnostic ignored "-Wcast-qual"
-#pragma clang diagnostic ignored "-Wchar-subscripts"
-#pragma clang diagnostic ignored "-Wconstant-logical-operand"
-#pragma clang diagnostic ignored "-Wcovered-switch-default"
-#pragma clang diagnostic ignored "-Wdisabled-macro-expansion"
-#pragma clang diagnostic ignored "-Wfloat-equal"
-#pragma clang diagnostic ignored "-Wformat-nonliteral"
-#pragma clang diagnostic ignored "-Wimplicit-int-enum-cast"
-#pragma clang diagnostic ignored "-Wimplicit-void-ptr-cast"
-#pragma clang diagnostic ignored "-Wpre-c11-compat"
-#pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
-#pragma clang diagnostic ignored "-Wunused-macros"
-#pragma clang diagnostic ignored "-Wused-but-marked-unused"
+  #pragma clang diagnostic warning "-Weverything"
+  #pragma clang diagnostic ignored "-Wconstant-logical-operand"
+  #pragma clang diagnostic ignored "-Wassign-enum"
+  #pragma clang diagnostic ignored "-Wc++-keyword"
+  #pragma clang diagnostic ignored "-Wc++98-compat"
+  #pragma clang diagnostic ignored "-Wcast-qual"
+  #pragma clang diagnostic ignored "-Wchar-subscripts"
+  #pragma clang diagnostic ignored "-Wconstant-logical-operand"
+  #pragma clang diagnostic ignored "-Wcovered-switch-default"
+  #pragma clang diagnostic ignored "-Wdisabled-macro-expansion"
+  #pragma clang diagnostic ignored "-Wfloat-equal"
+  #pragma clang diagnostic ignored "-Wformat-nonliteral"
+  #pragma clang diagnostic ignored "-Wimplicit-int-enum-cast"
+  #pragma clang diagnostic ignored "-Wimplicit-void-ptr-cast"
+  #pragma clang diagnostic ignored "-Wpre-c11-compat"
+  #pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
+  #pragma clang diagnostic ignored "-Wunused-macros"
+  #pragma clang diagnostic ignored "-Wused-but-marked-unused"
 #endif
 
+#if DEBUGGING || TESTING
+  #if CC_GCC || CC_CLANG
+    #pragma GCC diagnostic ignored "-Wunused-function"
+  #endif
+#endif
 
 #endif
