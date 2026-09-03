c7f2609edefae679c2b635748b2dc7855be71e94
lucas-mior <lucas.mior@tutamail.com>
2026-09-03 00:09:47 -0300

    cleanup

--- clipsim.h
+++ clipsim.h
@@ -15,8 +15,6 @@
 
 #include <X11/X.h>
 #include <X11/Xatom.h>
-#include <X11/Xatom.h>
-#include <X11/Xlib.h>
 #include <X11/Xlib.h>
 #include <X11/extensions/XInput2.h>
 #include <X11/extensions/Xfixes.h>
