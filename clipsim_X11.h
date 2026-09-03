f70b20b9612a521c507a79880ea3e748642f77a0
lucas-mior <lucas.mior@tutamail.com>
2026-09-02 23:55:46 -0300

    wrap x11 include

--- clipsim_X11.h
+++ clipsim_X11.h
@@ -1,9 +1,18 @@
 #if !defined(CLIPSIM_X11_H)
 #include "cbase.h"
 
+#if CC_CLANG
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreserved-macro-identifier"
+#endif
+
 #include <X11/X.h>
 #include <X11/Xatom.h>
 #include <X11/Xlib.h>
 #include <X11/extensions/Xfixes.h>
+
+#if CC_CLANG
+#pragma clang diagnostic pop
+#endif
 
 #endif /* CLIPSIM_X11_H */
