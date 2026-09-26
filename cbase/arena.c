c66057e8e7057ebcd9b6c3698f4ec924ae39c2c0
lucas-mior <lucas.mior@tutamail.com>
2026-09-26 10:32:15 -0300

    update cbase

--- cbase/arena.c
+++ cbase/arena.c
@@ -162,7 +162,7 @@ arena_back(Arena *arena, int64 size) {
 
     used = (char *)arena->pos - arena->begin;
     ASSERT_LESS_EQUAL(size, used);
-    ASSERT_ZERO(size % ALIGNMENT);
+    ASSERT_ZERO((ullong)size % ALIGNMENT);
 
     arena->pos = (char *)arena->pos - size;
     if (DEBUGGING && (size > 0)) {
